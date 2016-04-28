b0c5d3dae2746e75bb2b6dc17372534b910975e1
TheKiller65YT <jackgonser@gmail.com>
2016-04-27 14:40:25 -1000

    Sharing with emails!

--- js/app.js
+++ js/app.js
@@ -76,3 +76,14 @@ function toggle() {
   play = !play;
 }
 
+$(function () {
+      $('.SendEmail').click(function (event) {
+        var email = 'sample@gmail.com';
+        var subject = 'Test';
+        var emailBody = 'Hi Sample,';
+        var attach = 'path';
+        document.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody+"?attach="+attach;
+      });
+    });
+
+
